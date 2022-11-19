1422a869ec5593706d606bbc533e061a9f244f6d
Issi <issi.dev.iobroker@gmail.com>
2022-11-19 06:49:03 +0100

    add TreeTable example

--- admin/src/SettingPage.tsx
+++ admin/src/SettingPage.tsx
@@ -10,6 +10,7 @@ import {
 	Tooltip,
 	Typography,
 } from '@mui/material';
+import { TreeColumnProps, TreeDataProps, TreeTable } from 'iobroker-react';
 import { useI18n } from 'iobroker-react/hooks';
 import React from 'react';
 import { CustomTable } from './CustomTable';
@@ -48,20 +49,81 @@ function a11yProps(index: number) {
 		'aria-controls': `simple-tabpanel-${index}`,
 	};
 }
+
+const columns: TreeColumnProps[] = [
+	{
+		title: 'Name',
+		field: 'fieldIdInData',
+		editable: true,
+		subStyle: { color: '#00FF00' },
+		subField: 'subFieldData',
+	},
+	{ title: 'Text', field: 'text', editable: true, type: 'string' },
+	{ title: 'OID', field: 'oid', editable: true, type: 'oid' },
+	{ title: 'Color', field: 'color', editable: true, type: 'color' },
+	{ title: 'Numeric', field: 'numeric', editable: true, type: 'numeric' },
+	{ title: 'Boolean', field: 'boolean', editable: true, type: 'boolean' },
+	{
+		title: 'Select',
+		field: 'select',
+		editable: true,
+		type: 'string',
+		lookup: { 1: 'one', 2: 'two', 3: 'three' },
+	},
+];
+
+const newLine = {
+	id: Math.random().toString(36).substr(2, 9), // create random id
+	fieldIdInData: 'your data',
+	oid: 'myOID',
+	color: '#ff0000',
+	numeric: 123,
+	select: 'number',
+	boolean: true,
+	text: 'your data',
+};
+
 export const SettingPage: React.FC<SettingPageProps> = ({ changeSetting, settings }): JSX.Element => {
 	const { translate: _ } = useI18n();
 	const [value, setValue] = React.useState(0);
-	const [pass, setPassword] = React.useState(settings.password);
-
+	const [treeData, setTreeData] = React.useState<TreeDataProps[]>([
+		{
+			id: '1',
+			fieldIdInData: 'Test1',
+			oid: 'test.0.test1',
+			color: '#ff0000',
+			numeric: 1,
+			text: 'Text1',
+			boolean: true,
+			select: '1',
+		},
+		{
+			id: '2',
+			fieldIdInData: 'Test2',
+			oid: 'test.0.test2',
+			color: '#00ff00',
+			numeric: 2,
+			text: 'Text2',
+			parentId: '1',
+			subFieldData: 'Child',
+			boolean: false,
+			select: '2',
+		},
+		{
+			id: '3',
+			fieldIdInData: 'Test3',
+			oid: 'test.0.test3',
+			color: '#0000ff',
+			numeric: 3,
+			text: 'Text3',
+			boolean: true,
+			select: '3',
+		},
+	]);
 	const handleChange = (event: React.SyntheticEvent, newValue: number) => {
 		setValue(newValue);
 	};
 
-	// create a funktion to change password
-	const changePassword = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-		changeSetting('password', event.target.value);
-		setPassword(event.target.value);
-	};
 	return (
 		<React.Fragment>
 			<Box
@@ -175,6 +237,17 @@ export const SettingPage: React.FC<SettingPageProps> = ({ changeSetting, setting
 			</TabPanel>
 			<TabPanel index={4} value={value}>
 				<CustomTable setting={settings} onChange={(id, value) => changeSetting(id, value)} />
+				<br />
+				TreeTable
+				<TreeTable
+					columns={columns}
+					data={treeData}
+					newLineData={newLine}
+					newData={setTreeData}
+					noAdd={false}
+					noEdit={false}
+					noDelete={false}
+				/>
 			</TabPanel>
 			<TabPanel index={5} value={value}>
 				<Typography variant="h6" gutterBottom sx={{ display: 'flex', justifyContent: 'space-around' }}>
